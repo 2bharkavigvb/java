5785d1d0670a0f56659fd0f68ccc0b5cee59e14d
2bharkavigvb <43056929+2bharkavigvb@users.noreply.github.com>
2018-09-07 12:08:25 +0530

    Add files via upload

---
+++ methematical.java
@@ -0,0 +1,18 @@
+class Main 
+{ 
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+int a;
+double b,c,d,e,f;
+a=5;
+b=5;
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+System.out.println("The sum of two number is :"+c);
+System.out.println("The difference of two number is :"+d);
+System.out.println("The product of two number is:"+e);
+System.out.println("The division of two number is:"+f);
+  }
+}
